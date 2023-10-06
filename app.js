6b86245185245f68faf217a08f587b502a82348f
EvgeniiyaR <tulpansik@mail.ru>
2023-10-06 14:59:06 +0300

    feat: add solution

---
+++ app.js
@@ -0,0 +1,65 @@
+const products = [
+    {
+        id: 1,
+        title: 'Lenovo Yoga',
+        price: 3000,
+    },
+    {
+        id: 2,
+        title: 'Acer Aspire',
+        price: 1800,
+    },
+    {
+        id: 3,
+        title: 'Dell Vostro',
+        price: 3400
+    },
+];
+
+let order = [];
+
+function addToBasket(productId) {
+    products.forEach((product) => {
+        if (product.id === productId) {
+            if (order.some((item) => item.id === productId)) {
+                alert('Товар уже в корзине');
+            } else {
+                order = [...order, product];
+            }
+        }
+    })
+
+    // Эти строчки не трогаем, они отвечают за переотрисовку страницы
+    renderCart();
+    rerenderTotalPrice();
+}
+
+function removeFromBasket(productId) {
+    order = order.filter((item) => item.id !== productId);
+
+    // Эти строчки не трогаем, они отвечают за переотрисовку страницы
+    renderCart();
+    rerenderTotalPrice();
+}
+
+
+function rerenderTotalPrice() {
+    let totalPrice = 0;
+    order.forEach((item) => totalPrice += item.price);
+
+    // Не меняйте эту строчку
+    document.getElementById('total').innerText = totalPrice;
+}
+
+// Этот метод остается без изменений
+function renderCart() {
+    const cart = document.getElementById('basket-items');
+
+    cart.innerHTML = '';
+    order.forEach(item => {
+        const el = document.createElement('li');
+        el.innerText = item.title;
+        el.onclick = () => removeFromBasket(item.id);
+        cart.appendChild(el);
+    })
+}
