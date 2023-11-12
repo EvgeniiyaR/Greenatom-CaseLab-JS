0245769dcc0a35a581aee2d199ef68c883a2d498
EvgeniiyaR <tulpansik@mail.ru>
2023-11-12 20:10:34 +0300

    feat(filter): add Filter component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { ChangeEvent, useState, Dispatch } from "react";
+import { VehicleTypeSelect } from "./VehicleTypeSelect";
+import { VehicleType, Vehicle } from "../data/vehicles/contracts";
+import { VehicleApi } from "../data/vehicles/api";
+import { Input, Button, Gapped } from "@skbkontur/react-ui";
 
-export const Filter: React.FC = () => {
+interface IFilter {
+    setVehicles: Dispatch<Vehicle[]>;
+}
+
+export const Filter: React.FC<IFilter> = ({ setVehicles }) => {
+    const [selectedVehicleType, setSelectedVehicleType] = useState<VehicleType | null>(null);
+    const [searchValue, setSearchValue] = useState<string>('');
+
+    const handleVehicleTypeChange = (newType: VehicleType | null) => {
+        setSelectedVehicleType(newType);
+        setVehicles(VehicleApi.search({title: searchValue, type: newType}));
+    };
+
+    const handleSearchValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setSearchValue(value);
+        setVehicles(VehicleApi.search({title: value, type: selectedVehicleType}));
+    };
+
+    const hadnleSubmit = (event: ChangeEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setVehicles(VehicleApi.search({title: searchValue, type: selectedVehicleType}));
+    };
+
     return (
-        <></>
+        <form name="form-search" onSubmit={hadnleSubmit}>
+            <Gapped gap={5}>
+                <Input name="search" placeholder="Поиск" onChange={handleSearchValueChange} value={searchValue} />
+                <VehicleTypeSelect value={selectedVehicleType} onChange={handleVehicleTypeChange} />
+                <Button type="submit">Search</Button>
+            </Gapped>
+        </form>
     );
 };
